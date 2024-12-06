a23e03199c456f4dce6c7ed1ba0565ac15f7f801
RICHYDOS <richardosunmu@gmail.com>
2024-12-06 22:45:23 +0100

    chore: log database name when connected to the db

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { sequelizeConnection } from './core/database/sequelize';
 
 sequelizeConnection.sync().then(() => {
-	console.log('Connected to the Database');
+	console.log(`Connected to ${sequelizeConnection.getDatabaseName()} Database`);
 });
