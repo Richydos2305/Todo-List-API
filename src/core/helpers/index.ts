696172df01ffbf502a6e1f8c9f98ad4f8d4ac415
RICHYDOS <richardosunmu@gmail.com>
2024-12-12 08:14:38 +0100

    feat: more helper functions

--- src/core/helpers/index.ts
+++ src/core/helpers/index.ts
@@ -1,9 +1,37 @@
 import { Response } from 'express';
+import { settings } from '../config/application';
+import { sign } from 'jsonwebtoken';
+import { Tasks } from '../models/tasks';
+import { Users } from '../models/users';
 
-export function handleError(res: Response, statusCode: number, message: string, title: string = 'Error'): never {
-  res.status(statusCode).send({
-    Title: title,
-    Message: message,
-  });
-  throw new Error(message);
+export function handleError(res: Response, statusCode: number, message: string): void {
+  res.status(statusCode).send({ message });
+}
+
+export function getAccessToken(user: {name: string, email: string, id: number}): string {
+
+  const accessToken = sign(
+    {
+      userDetails: {
+        name: user.name,
+        email: user.email,
+        id: user.id
+      }
+    },
+    settings.secretKey,
+    { expiresIn: '4h' }
+  );
+  return accessToken
+}
+
+export function isAuthorizedUser(task: Tasks, loggedInUserId: number ): boolean {
+  if (task.user_id === loggedInUserId)
+    return true
+  return false
+}
+export async function userExists(loggedInUserId: number ): Promise<boolean> {
+  const loggedInUser = await Users.findOne({ where: { id:loggedInUserId } })
+  if (loggedInUser)
+    return true
+  return false
 }
