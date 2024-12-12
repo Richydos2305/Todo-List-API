fd707afd72eec7dfa5b9817d4963235a3000c7b1
RICHYDOS <richardosunmu@gmail.com>
2024-12-12 08:13:16 +0100

    feat: server setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
 import { sequelizeConnection } from './core/database/sequelize';
+import express from 'express';
+import { settings } from './core/config/application';
+import routes from './core/routes';
+import errorHandler from './core/middleware/errorhandler';
+
+const app = express();
+const port = settings.port || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+	console.log(`Server running on Port ${port}`);
+});
 
 sequelizeConnection.sync().then(() => {
 	console.log(`Connected to ${sequelizeConnection.getDatabaseName()} Database`);
