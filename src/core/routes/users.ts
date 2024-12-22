298a3f00497a16746b0656221319e069f3653816
RICHYDOS <richardosunmu@gmail.com>
2024-12-22 18:48:58 +0100

    fix: setup endpoints for cron job

--- src/core/routes/users.ts
+++ src/core/routes/users.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { asyncHandler } from '../middleware/asyncHandler';
-import { signup, login } from '../controllers/users';
+import { signup, login, welcome } from '../controllers/users';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', asyncHandler(signup));
 userRouter.post('/login', asyncHandler(login));
+userRouter.get('/', asyncHandler(welcome));
 
 export default userRouter;
